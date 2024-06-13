1858f17eb74ca68ce76ff31f859473e2ee3b1bd9
uzair-hijazi <suikh23@gmail.com>
2024-06-13 22:28:58 +0500

    Currency Converter

---
+++ index.js
@@ -0,0 +1,61 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    PKR: 279.33,
+    GBP: 0.78,
+    EUR: 0.93,
+    RYL: 3.75,
+    DIR: 3.67,
+    AUD: 1.51,
+    YUAN: 7.25,
+    YEN: 156.91,
+    LIRA: 32.34,
+};
+let frmCurrency = await inquirer.prompt([
+    {
+        name: "currencyfrom",
+        message: "Enter Currency to convert from:",
+        type: "list",
+        choices: [
+            "USD",
+            "PKR",
+            "GBP",
+            "EUR",
+            "RYL",
+            "DIR",
+            "AUD",
+            "YUAN",
+            "YEN",
+            "LIRA",
+        ],
+    },
+    {
+        name: "currencyto",
+        message: "Enter Currency to convert into:",
+        type: "list",
+        choices: [
+            "USD",
+            "PKR",
+            "GBP",
+            "EUR",
+            "RYL",
+            "DIR",
+            "AUD",
+            "YUAN",
+            "YEN",
+            "LIRA",
+        ],
+    },
+    {
+        name: "amount",
+        message: "Enter amount to convert:",
+        type: "number",
+    },
+]);
+let frmAmount = currency[frmCurrency.currencyfrom];
+let toAmount = currency[frmCurrency.currencyto];
+let amountAnswer = frmCurrency.amount;
+let baseAmount = amountAnswer / frmAmount;
+let cnvrtdAmount = baseAmount * toAmount;
+console.log(cnvrtdAmount);
